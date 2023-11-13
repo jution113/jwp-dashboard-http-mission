5d6fc9aa3138bcfe40520bc65596beeee0c745da
jution113 <jution113@gmail.com>
2023-11-14 01:35:20 +0900

    Feat: create CssController

--- tomcat/src/main/java/nextstep/jwp/controller/CssController.java
+++ tomcat/src/main/java/nextstep/jwp/controller/CssController.java
@@ -1,2 +1,23 @@
-package nextstep.jwp.controller;public class CssController {
+package nextstep.jwp.controller;
+
+import nextstep.jwp.Response.Response;
+import nextstep.jwp.Response.ResponseBody;
+import nextstep.jwp.Response.ResponseHeader;
+import nextstep.jwp.request.Request;
+import nextstep.jwp.util.ResourceFinder;
+import org.apache.util.HttpResponseCode;
+
+import java.io.IOException;
+
+public class CssController {
+    // url: /css
+    public Response handleRequest(Request request) throws IOException {
+        ResourceFinder resourceFinder = new ResourceFinder();
+        ResponseBody responseBody = new ResponseBody(resourceFinder.getResource(request.getRequestHeader().getPath()));
+        ResponseHeader responseHeader = new ResponseHeader(String.valueOf(HttpResponseCode.OK.getCode()),
+                HttpResponseCode.OK.getReasonPhrase(),
+                resourceFinder.getContentType(resourceFinder.getFileExtension(request.getRequestHeader().getPath())),
+                responseBody.getLength());
+        return new Response(responseHeader, responseBody);
+    }
 }
