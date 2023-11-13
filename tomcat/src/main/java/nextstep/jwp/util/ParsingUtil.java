1859ec5f6ae246aa4512fce2f7c144742dbb87f4
jution113 <jution113@gmail.com>
2023-11-13 17:47:06 +0900

    Refactor: Modify parseRequest()

--- tomcat/src/main/java/nextstep/jwp/util/ParsingUtil.java
+++ tomcat/src/main/java/nextstep/jwp/util/ParsingUtil.java
@@ -9,32 +9,37 @@ import java.io.*;
 public class ParsingUtil {
     public Request parseRequest(InputStream inputStream) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-        String requestLine = reader.readLine();
+        try {
+            String requestLine = reader.readLine();
+            String[] requestParts = requestLine.split(" ");
+            String method = requestParts[0];
+            String path = requestParts[1];
+            String contentType = "";
+            int contentLength = 0;
 
-        String[] requestParts = requestLine.split(" ");
-        String method = requestParts[0];
-        String path = requestParts[1];
-        String contentType = "";
-        int contentLength = 0;
-
-        String line;
-        while ((line = reader.readLine()) != null && !line.isEmpty()) {
-            if (line.startsWith("Content-Type: ")) {
-                contentType = line.substring("Content-Type: ".length());
-            } else if (line.startsWith("Content-Length: ")) {
-                contentLength = Integer.parseInt(line.substring("Content-Length: ".length()));
+            String line;
+            while ((line = reader.readLine()) != null && !line.isEmpty()) {
+                if (line.startsWith("Content-Type: ")) {
+                    contentType = line.substring("Content-Type: ".length());
+                } else if (line.startsWith("Content-Length: ")) {
+                    contentLength = Integer.parseInt(line.substring("Content-Length: ".length()));
+                }
             }
+
+            RequestHeader requestHeader = new RequestHeader(method, path, contentType, contentLength);
+
+            StringBuilder bodyBuilder = new StringBuilder();
+            if(contentLength> 0) {
+                while ((line = reader.readLine()) != null && !line.isEmpty()) {
+                    bodyBuilder.append(line).append("\r\n");
+                }
+            }
+
+            RequestBody requestBody = new RequestBody(bodyBuilder.toString());
+
+            return new Request(requestHeader, requestBody);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-
-        RequestHeader requestHeader = new RequestHeader(method, path, contentType, contentLength);
-
-        StringBuilder bodyBuilder = new StringBuilder();
-        while ((line = reader.readLine()) != null) {
-            bodyBuilder.append(line).append("\r\n");
-        }
-
-        RequestBody requestBody = new RequestBody(bodyBuilder.toString());
-
-        return new Request(requestHeader, requestBody);
     }
 }
