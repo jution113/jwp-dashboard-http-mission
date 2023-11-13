374c2edef2081ce3284df760a3cf776b4d9705f2
jution113 <jution113@gmail.com>
2023-11-14 01:37:05 +0900

    Refactor: findController() for add path

--- tomcat/src/main/java/org/apache/coyote/http11/ControllerMapper.java
+++ tomcat/src/main/java/org/apache/coyote/http11/ControllerMapper.java
@@ -1,9 +1,12 @@
 package org.apache.coyote.http11;
 
 import nextstep.jwp.Response.Response;
-import nextstep.jwp.controller.IndexController;
-import nextstep.jwp.controller.LoginController;
+import nextstep.jwp.Response.ResponseBody;
+import nextstep.jwp.Response.ResponseHeader;
+import nextstep.jwp.controller.*;
 import nextstep.jwp.request.Request;
+import nextstep.jwp.util.ResourceFinder;
+import org.apache.util.HttpResponseCode;
 
 import java.io.IOException;
 
@@ -22,9 +25,27 @@ public class ControllerMapper {
         } else if(path.startsWith("/register")) {
             return new Response();
 
-        } else {
-            return new Response();
+        } else if(path.startsWith("/css")) {
+            CssController cssController = new CssController();
+            return cssController.handleRequest(request);
 
+        } else if(path.startsWith("/js")) {
+            JsController jsController = new JsController();
+            return jsController.handleRequest(request);
+
+        } else if(path.startsWith("/assets")) {
+            AssetsController assetsController = new AssetsController();
+            return assetsController.handleRequest(request);
+
+        } else {
+            ResourceFinder resourceFinder = new ResourceFinder();
+            ResponseBody responseBody = new ResponseBody(resourceFinder.getResource(request.getRequestHeader().getPath()));
+            ResponseHeader responseHeader = new ResponseHeader(String.valueOf(HttpResponseCode.NOT_FOUND.getCode()),
+                    HttpResponseCode.NOT_FOUND.getReasonPhrase(),
+                    resourceFinder.getContentType(resourceFinder.getFileExtension(request.getRequestHeader().getPath())),
+                    responseBody.getLength());
+
+            return new Response(responseHeader, responseBody);
         }
     }
 }
